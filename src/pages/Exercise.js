afe5c51a8741df60b223a4d8be6c69cc79c895ac
Naimur <naeemhasan28@gmail.com>
2022-10-15 09:56:53 +0600

    added variants

--- src/pages/Exercise.js
+++ src/pages/Exercise.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { motion } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion/dist/framer-motion'
 
 const Exercise = () => {
+  const [isVisible, setisVisible] = useState(false)
   return (
-    <section class="container-full">
+    <section className="container-full">
       <section className="section">
+        <p>Easing</p>
         <motion.div
           className="w-32 h-32 bg-red-500 rounded"
           animate={{ x: 200 }}
           transition={{ easing: 'easeOut', duration: 0.5 }}
         ></motion.div>
+      </section>
+      <section className="section">
+        <p>Initial false</p>
+        <motion.div
+          className="w-32 h-32 bg-red-500 rounded"
+          animate={{ x: 200 }}
+          initial={false}
+        ></motion.div>
+      </section>
+      <section className="section">
+        <AnimatePresence>
+          {isVisible && (
+            <motion.div
+              className="w-32 h-32 bg-red-500 rounded"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            />
+          )}
+        </AnimatePresence>
+        <button onClick={() => setisVisible((isVisible) => !isVisible)}>
+          Toggle Show
+        </button>
       </section>
     </section>
   )
